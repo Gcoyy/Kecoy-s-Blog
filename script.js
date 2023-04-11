bd9621de3c0b16c6974e4a8c90baa87a5f134885
Gcoyy <116942403+Gcoyy@users.noreply.github.com>
2023-04-11 16:58:45 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.getElementById('login-form').addEventListener('submit', function(event)
     const password = document.getElementById('password').value;
     // Replace the following with your actual login logic
     if (username === 'Kecoy' && password === '3162023') {
-      window.location.href = 'dashboard.html'; // Redirect to dashboard page
+      window.location.href = 'home.html'; // Redirect to dashboard page
     } else {
       document.getElementById('error-msg').innerText = 'Invalid username or password';
     }
