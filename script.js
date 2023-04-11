14605db78c05b5abb1db485d12f5b0e7d0571c99
Gcoyy <116942403+Gcoyy@users.noreply.github.com>
2023-04-11 16:52:00 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    // Replace the following with your actual login logic
+    if (username === 'Kecoy' && password === '3162023') {
+      window.location.href = 'dashboard.html'; // Redirect to dashboard page
+    } else {
+      document.getElementById('error-msg').innerText = 'Invalid username or password';
+    }
+  });
